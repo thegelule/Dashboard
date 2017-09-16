1583076ed7835d51f16b1fe1fc37838358c11b2b
emesh <thomas.gely974@gmail.com>
2017-09-16 11:15:46 +0200

    remplacement iframe par ajax

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,12 +1,19 @@
 function OpenCampaignCharacters(campString){
     var frame = document.getElementById("ContentDisplayFrame");
     
-    resizeIframe(frame);
+    //resizeIframe(frame);
     
     switch(campString){
         
         case "GITS":
-            frame.src = "pages/GITS_characters.html";
+            //frame.src = "pages/GITS_characters.html";
+			$.ajax({
+				url : "/pages/GITS_characters.html",
+				success : function(result){
+					var asyncContentContainer = document.getElementById("ContentDisplayContainer");
+					asyncContentContainer.innerHTML = result;
+				}
+			});
             break;
     }
     
@@ -14,5 +21,8 @@ function OpenCampaignCharacters(campString){
 }
 
 function resizeIframe(obj) {
-    obj.style.height = window.getComputedStyle(obj.contentWindow.document.body).inlineSize;
+	var pageContentHeight = window.getComputedStyle(document.getElementsByClassName("page-content")[0]).height.replace("px","");
+	var pageContentPaddingB = window.getComputedStyle(document.getElementsByClassName("page-content")[0]).paddingBottom.replace("px","");
+	
+    obj.style.height = parseInt(pageContentHeight) - parseInt(pageContentPaddingB) + "px";
   }
