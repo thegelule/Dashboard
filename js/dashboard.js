5ac9eca251ea0b0729e3960dc406e1c1acf1696d
emesh <thomas.gely974@gmail.com>
2017-09-16 12:50:06 +0200

    ajout affichage ajax nb persos

--- js/dashboard.js
+++ js/dashboard.js
@@ -26,4 +26,29 @@ function resizeIframe(obj) {
 	var pageContentPaddingB = window.getComputedStyle(document.getElementsByClassName("page-content")[0]).paddingBottom.replace("px","");
 	
     obj.style.height = parseInt(pageContentHeight) - parseInt(pageContentPaddingB) + "px";
-  }
+ }
+  
+function DisplayNumbersInMenu(){
+	FetchNumberofCharacters("GITS_characters.html");
+}
+
+function FetchNumberofCharacters(pageName){
+	var urlToFetch = "https://thegelule.github.io/Dashboard/pages/" + pageName;
+	var tab = pageName.slice("_");
+	var campName = tab[0];
+	
+	$.ajax({
+		url : urlToFetch,
+		type: "GET",
+		success : function(result){
+			var count = result.match(/mt-card-item/g) || []).length;
+			document.getElementById(campName + "CharacNb").innerText = count;
+		}
+	});
+}
+
+function CountOccurencesInString(string, subString){
+	var result = null;
+	
+	(
+}
