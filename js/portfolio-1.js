e9d6336a7c8e598ada3d412be615f1f630f50862
emesh <thomas.gely974@gmail.com>
2017-09-30 18:19:27 +0200

    attempt fix portfolio

--- js/portfolio-1.js
+++ js/portfolio-1.js
@@ -1,4 +1,4 @@
-(function($, window, document, undefined) {
+function InitPortfolio($, window, document, undefined) {
     'use strict';
 
     // init cubeportfolio
@@ -62,4 +62,4 @@
         },
     });
     
-})(jQuery, window, document);
+};
