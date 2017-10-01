08a4c89237e23a5a9b986bd6a983823de2c6a35a
emesh <thomas.gely974@gmail.com>
2017-10-01 14:52:40 +0200

    added character sheet

--- js/dashboard.js
+++ js/dashboard.js
@@ -83,3 +83,134 @@ function LoadHomePage(){
         }
     });
 }
+
+function OpenCharacterDialog(element){
+    var characID = element.getAttribute("data-id");
+    var character = RetrieveCharacterFromID(characID);
+    
+    $.ajax({
+       url: "https://thegelule.github.io/Dashboard/pages/CharacterSheet.html",
+        type: "GET",
+        success : function(result){
+            var bufferElement = document.createElement("div");
+            bufferElement.innerHTML = result;
+            FillForms(bufferElement, character);
+            bootbox.alert(bufferElement.innerHTML);
+        }
+    });
+}
+
+function FillForms(e,ch){
+    FillAspects(e.getElementById("AspectsContainer"),ch.aspects);
+    FillSkills(e.getElementById("SkillsContainer"),ch.skills);
+    FillStunts(e.getElementById("StuntsContainer"),ch.stunts);
+    FillAbilities(e.getElementById("AbilitiesContainer"),ch.abilities);
+}
+
+function FillAspects(e,aspects){
+    var aspectContainers = e.getElementsByClassName("aspect");
+    
+    for(var i = 0; i < aspects.length; i++){
+        var aspectElement = aspectContainers[i];
+        var aspectText = aspects[i];
+        aspectElement.value = aspectText;
+    }
+}
+
+function FillSkills(e,skills){    
+    for(var i = 0; i < skills.length; i++){
+        var skillLevelTab = skills[i];
+        var increment = 5 - (i+1);
+        var level = "level" + increment;
+        var levelContainerElement = e.getElementsByClassName(level);
+        var skillsElements = levelContainerElement.getElementsByClassName("skills");
+        
+       for(var j = 0; j < skillLevelTab.length; j++){
+           var skill = skillLevelTab[j];
+           var skillElement = skillsElements[j];
+           
+           if(skill != undefined){
+               skillElement.value = skill;
+           }
+       } 
+    }
+}
+
+function FillStunts(e,stunts){
+    var formStunts = e.getElementsByClassName("form")[0];
+    
+    for(var i = 0; i < stunts.length; i++){
+        var stunt = stunts[i];
+        var stuntElement = NewStuntElement(stunt);
+        formStunts.appendChild(stuntElement);
+    }
+}
+
+function FillAbilities(e,abilities){
+    for(var i = 0; i < abilities.length; i++){
+        var levelTab = abilities[i];
+        var classParameter = "level" + (i+1);
+        var levelElement = document.getElementsByClassName(classParameter)[0];
+        var abilitiesContainer = levelElement.getElementsByClassName("form")[0];
+        
+        for(var j = 0; j < levelTab.length; j++){
+            var ability = levelTab[j];
+            var abilityElement = NewAbilityElement(ability);
+            abilitiesContainer.appendChild(abilityElement);
+        }
+    }
+}
+
+function RetrieveCharacterFromID(ID){
+    var charactersList = globalDB.players;
+    var returnedCharacter = null;
+    
+    for(var i = 0; i < charactersList.length; i++){
+        var character = charactersList[i];
+        if(character.playerID == ID){
+            returnedCharacter = character;
+            break;
+        }
+    }
+           
+    return returnedCharacter;
+}
+
+function NewStuntElement(stunt){
+    var formGroup = document.createElement("div");
+    var label = document.createElement("label");
+    var textContainerParent = document.createElement("div");
+    var textContainer = document.createElement("p");
+    
+    formGroup.classList.add("form-group");
+    label.classList.add("stuntTitle");
+    label.innerHTML = stunt.title;
+    textContainerParent.classList.add("col-md-4");
+    textContainer.classList.add("form-control-static");
+    textContainer.classList.add("stuntText");
+    textContainer.innerHTML = stunt.text;
+    
+    textContainerParent.appendChild(textContainer);
+    formGroup.appendChild(label);
+    formGroup.appendChild(textContainerParent);
+    
+    return formGroup;
+}
+
+function NewAbilityElement(ability){
+    var formGroup = document.createElement("div");
+    var textContainerParent = document.createElement("div");
+    var textContainer = document.createElement("p");
+    
+    formGroup.classList.add("form-group");
+    textContainerParent.classList.add("col-md-12");
+    textContainer.classList.add("form-control-static");
+    textContainer.classList.add("ability");
+    textContainer.innerHTML = ability;
+    
+    textContainerParent.appendChild(textContainer);
+    formGroup.appendChild(label);
+    formGroup.appendChild(textContainerParent);
+    
+    return formGroup;
+}
--- js/globalDB.js
+++ js/globalDB.js
@@ -30,7 +30,23 @@ var globalDB = {
         level: 4,
         earnedXP: 15,
         divineParent: "http://wyrdwalkers-fr.wikidot.com/quetzalcoatl",
-        charWikiLink: "http://wyrdwalkers-fr.wikidot.com/dayana-citlalin"
+        charWikiLink: "http://wyrdwalkers-fr.wikidot.com/dayana-citlalin",
+        aspects: ["Archéologue aventurière", "Oeillères passionnées", "Douée de ses mains", "Protectrice de l'Histoire", "Le charisme des profondeurs"],
+        skills: [[""], ["Volonté", ""],["Savoir","Athlétisme",""],["Rapport","Anatomie","Combat",""]],
+        stunts:[{
+            title:"Mains calleuses",
+            text: "Peut utiliser Anatomie au corps-à-corps en attaque avec un bonus de +1"
+        },{
+            title:"Esprit d'acier",
+            text: "Peut utiliser Volonté pour se défendre"
+        },{
+            title:"Don du sang",
+            text: "Lors d'un jet d'Hématurgie, choisir : <br><ul><li>+1 au prochain de Volonté</li><li>Le prochain aspect créé par Hématurgie donne +3 au lieu de +2</li></ul>"
+        },{
+            title:"L'esprit et le corps",
+            text: "Peut utiliser Volonté pour les jets d'initiative"
+        }],
+        abilities:[["Hématurgie - Itzli", "Magie des âmes - Soulbinding", "Vision du Wyrd - Mystère", "Physiologie Epique - Ouïe", "Nuit - Lune", "Terre - Montagne", "Profondeurs - Cavernes", "Ordre - Gravité", "Taiyi - Nature", "Druidisme - Sorcellerie Fey (Eté)", "Enech - Aventure<br><ul><li>Lumière - Soleil</li><ul>"],[],[]]
     },{
         playerID: 2,
         name: "Melissa Apidae",
