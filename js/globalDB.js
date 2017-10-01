15039d907dee70a6142abb4c7d5d82c4d6a5abbd
emesh <thomas.gely974@gmail.com>
2017-10-01 10:13:18 +0200

    added db js

---
+++ js/globalDB.js
@@ -0,0 +1,70 @@
+var globalDB = {
+    campaigns:[{
+        name: "Ghosts In The Wind",
+        nbPlayers: 6,
+        players: [1,2,3,4,5,6],
+        running: true,
+        done: false
+    },{
+        name: "Bringing Steel To The Sky",
+        nbPlayers: 4,
+        players: [11,12,13,14],
+        running: false,
+        done: true
+    },{
+        name: "Godly Check Mate",
+        nbPlayers: 5,
+        players: [11,12,13,14,15],
+        running: true,
+        done: false
+    },{
+        name: "Wind of Change",
+        nbPlayers: 4,
+        players: [7,8,9,10],
+        running: false,
+        done: false
+    }],
+    players:[{
+        playerID: 1,
+        name: "Dayana Citlalin",
+        level: 4,
+        earnedXP: 15,
+        divineParent: "http://wyrdwalkers-fr.wikidot.com/quetzalcoatl",
+        charWikiLink: "http://wyrdwalkers-fr.wikidot.com/dayana-citlalin"
+    },{
+        playerID: 2,
+        name: "Melissa Apidae",
+        level: 4,
+        earnedXP: 15,
+        divineParent: "http://wyrdwalkers-fr.wikidot.com/parvati",
+        charWikiLink: "http://wyrdwalkers-fr.wikidot.com/melissa-apidae"
+    },{
+        playerID: 3,
+        name: "Sybil McConnell",
+        level: 4,
+        earnedXP: 15,
+        divineParent: "http://wyrdwalkers-fr.wikidot.com/dian-cecht",
+        charWikiLink: "http://wyrdwalkers-fr.wikidot.com/sybil-mcconnell"
+    },{
+        playerID: 4,
+        name: "Wilfrid Krain",
+        level: 4,
+        earnedXP: 15,
+        divineParent: "http://wyrdwalkers-fr.wikidot.com/tyr",
+        charWikiLink: "http://wyrdwalkers-fr.wikidot.com/wilfrid-krain"
+    },{
+        playerID: 5,
+        name: "Ljubomir 'Mimir' Uspešno",
+        level: 4,
+        earnedXP: 15,
+        divineParent: "http://wyrdwalkers-fr.wikidot.com/chernobog",
+        charWikiLink: "http://wyrdwalkers-fr.wikidot.com/ljubomir-uspesno"
+    },{
+        playerID: 6,
+        name: "Sullivan O'Neill",
+        level: 4,
+        earnedXP: 15,
+        divineParent: "http://wyrdwalkers-fr.wikidot.com/hades",
+        charWikiLink: "http://wyrdwalkers-fr.wikidot.com/sullivan-oneill"
+    }]
+}
