dc6f5dbebcbc5909a424ba097b20c4306e2e877c
jayce89 <jacen.shan@gmail.com>
2017-12-17 17:38:27 +0100

    js fix

--- js/dashboard.js
+++ js/dashboard.js
@@ -1213,7 +1213,7 @@ function EmbedPDF(PDFurl){
     var ContentContainer = $("#ContentDisplayContainer");
     var PDFContainer = document.createElement("div");
     var options = {
-        height: parseInt($(".page-content-wrapper > .page-content")[0].style.minHeight) - parseInt($(".page-footer").css("height")) - 5 + "px"
+        height: parseInt($(".page-content-wrapper > .page-content")[0].style.minHeight) - parseInt($(".page-footer").css("height")) - 6 + "px"
     }
 
     ContentContainer.empty();
