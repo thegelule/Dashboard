1728fd0813516d704ac48669c5962cd4067b8609
jayce89 <jacen.shan@gmail.com>
2017-12-21 20:40:15 +0100

    upload pdf conversion library

--- js/dashboard.js
+++ js/dashboard.js
@@ -1224,3 +1224,36 @@ function EmbedPDF(PDFurl){
 
     PDFObject.embed(PDFurl, "#PDFContainer", options);
 }
+
+/*********** PDF Sheet Generator *****************/
+
+var form = $('.form');
+var cache_width = form.width();
+var a4 = [595.28, 841.89]; // for a4 size paper width and height
+
+function ConvertToPDF(element){
+    $('body').scrollTop(0);
+    createPDF();
+}
+
+function createPDF(){
+    getCanvas().then(function(canvas) {
+        var
+            img = canvas.toDataURL("image/png"),
+            doc = new jsPDF({
+            unit: 'px',
+            format: 'a4'
+            });
+        doc.addImage(img, 'JPEG', 20, 20);
+        doc.save('Aesir.pdf');
+        form.width(cache_width);
+    });
+}
+
+function getCanvas() {
+    form.width((a4[0] * 1.33333) - 80).css('max-width', 'none');
+    return html2canvas(form[0], {
+    imageTimeout: 2000,
+    removeContainer: true
+    });
+}
