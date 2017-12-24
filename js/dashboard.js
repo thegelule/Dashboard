d359806ac9f94b4650aa765ad615de20b91a5b51
emesh <thomas.gely974@gmail.com>
2017-12-24 10:32:10 +0100

    css fix

--- js/dashboard.js
+++ js/dashboard.js
@@ -1425,3 +1425,18 @@ function getCanvas(pageSelector) {
 }
 
 
+function printElem(divId) {
+    var content = document.getElementById(divId).innerHTML;
+    var mywindow = window.open('', 'Print', 'height=600,width=800');
+
+    mywindow.document.write('<html><head><title>Print</title>');
+    mywindow.document.write('</head><body >');
+    mywindow.document.write(content);
+    mywindow.document.write('</body></html>');
+
+    mywindow.document.close();
+    mywindow.focus()
+    mywindow.print();
+    mywindow.close();
+    return true;
+}
